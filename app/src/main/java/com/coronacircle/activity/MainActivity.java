0beefb1c54c3d0cc7885659bcc2eceedcc6ccc5f
minj-L <52521457+minj-L@users.noreply.github.com>
2020-10-06 19:06:11 +0900

    최초 실행여부 판단후 권환화면 띄우기 기능 추가

--- app/src/main/java/com/coronacircle/activity/MainActivity.java
+++ app/src/main/java/com/coronacircle/activity/MainActivity.java
@@ -3,6 +3,9 @@ package com.coronacircle.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -29,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
 //        viewpagerContent = findViewById(R.id.viewPager);
 //        tabContent = findViewById(R.id.tab);
 //        tabContent.setupWithViewPager(viewpagerContent);
+
+        //최초실행여부 판단
+        SharedPreferences pref = getSharedPreferences("isFirst", Activity.MODE_PRIVATE);
+        int isFirst = pref.getInt("First",0);
+        if(isFirst != 1){
+            Intent intent = new Intent(MainActivity.this, PermissionCheckActivity.class);
+            startActivity(intent);
+        }
 
         loadTabName();
         setTabLayout();
